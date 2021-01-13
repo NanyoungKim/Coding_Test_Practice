103764198ff6b598e0e3d8a2bdbedced5fc9cf5a
Nanyoung Kim <nanyoung18@gmail.com>
2021-01-13 20:14:28 +0900

    BFS로 해결

--- Coding_Test_Practice/DFS_BOJ2644_촌수계산.cpp
+++ Coding_Test_Practice/DFS_BOJ2644_촌수계산.cpp
@@ -1,75 +1,66 @@
 
 #include <iostream>
-#include <cstring>
+#include <queue>
 #include <vector>
 using namespace std;
 
-int n,m;
-int node1, node2;
-int parent, child;
 
-int parentArr[101] = {0};
+queue<int> que;
+int fromStart[101];
+vector<int> connected[101];     //연결된 노드들이 벡터에 저장되어 각 배열에 들어있음
 
-
-vector<int> findParent(int child){
+int BFS(int start, int end){
     
-    vector<int> vec;
-    int sum = 0;
-    
-    while(1){
-            
-        int parent = parentArr[child];
-        if(parent == 0) {
-            
-            vec.push_back(child);    //루트노드
-            vec.push_back(sum);     //그 루트노드까지 거슬러 올라간 길이(촌수)
-            break;
-        }                           //거슬러 올라가다 node2를 만나버리면
-        else if(child==node2){
-            vec.push_back(-1);
-            vec.push_back(sum);
-            break;
+    while(!que.empty()){
+        
+        int x = que.front();
+        que.pop();
+        
+        if(x==end){                     //타겟 노드 찾으면 시작노드부터 그 노드까지 거리 리턴
+            return fromStart[x];
         }
+        
         else{
-            sum+=1;
-            child = parent;
+            for(int i = 0; i<connected[x].size(); i++){
+                int next = connected[x][i];
+                
+                if(fromStart[next]==0){ //아직 방문 안한 곳
+                    que.push(next);
+                    fromStart[next] = fromStart[x] + 1;
+                }
+            }
         }
     }
-    return vec;
-}
+    
+    return -1;
 
+}
 
 
 int main(){
     
     int answer = 0;
-    
-    cin>>n;
-    cin>>node1 >> node2;
-    cin>>m;
+    int n,m;
+    int node1, node2;
+    int parent, child;
 
+    
+    cin >> n >> node1 >> node2 >> m;
+
+    
     for(int i = 0; i<m; i++){
         cin >> parent >> child;
-        parentArr[child] = parent;
-    }
+        connected[parent].push_back(child);     //양방향 연결
+        connected[child].push_back(parent);
 
-    
-    vector<int> ansVec1 = findParent(node1);
-    vector<int> ansVec2 = findParent(node2);
-    
-    if(ansVec1[0]==-1){             //거슬러 올라가다가 만난
-        answer = ansVec1[1];
-    }
-    else{
-        if(ansVec1[0]!=ansVec2[0]){
-            answer = -1;
-        }
-        else{
-            answer = ansVec1[1] + ansVec2[1];
-        }
     }
     
-
+    que.push(node1);
+    
+    answer = BFS(node1,node2);
+    
+    
+    
     cout << answer << endl;
   
     return 0;
