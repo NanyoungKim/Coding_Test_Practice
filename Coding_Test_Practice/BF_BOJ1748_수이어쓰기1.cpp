133a5739c2dd1a96d424c9deffdd7ad7b42110fa
Nanyoung Kim <nanyoung18@gmail.com>
2021-01-28 09:22:03 +0900

    N을 10^x로 나눈 몫을 이용하자

--- Coding_Test_Practice/BF_BOJ1748_수이어쓰기1.cpp
+++ Coding_Test_Practice/BF_BOJ1748_수이어쓰기1.cpp
@@ -3,42 +3,27 @@
 
 using namespace std;
 
-
 int main(){
     
     
-    int N, ans = 0;
-    cin>>N;
-    int x = 0;
-    cout << pow(10,0) << endl;
-    int r;
-    if(N>=10){
-
-        for(int x = 0; ; x++){
-
-            r = pow(10.0,x);
-            if(N / r  >= x) {x++;}
-            else break;
-        }
-
-
-        for(int i = 1; i<=x; i++){
-
-            int p = pow(10.0, i-1);
-            ans += 9*p * i;
-
-        }
-
-               
-        ans += (N-r + 1) * (x+1);
-    }
-
-    else{
-        ans = N;
-    }
-
-    cout << ans << "\n";
-
+    int N;
+    cin >> N;
+    int x = 1;
+    int ans = 0;
+    while(1){
     
+        int q = N/pow(10,x);
+        if(q>0){
+            ans += (pow(10,x)-pow(10,x-1)) * x;
+        }
+        else{
+            ans += (N-pow(10,x-1) + 1) * x;
+            break;
+        }
+        x++;
+    }
+    
+    cout  << ans << "\n";
+
     return 0;
 }
