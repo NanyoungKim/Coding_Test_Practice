3c79faf6dd6dcb82be51927505fbe6fb7d63ba37
Nanyoung Kim <nanyoung18@gmail.com>
2021-02-07 08:50:46 +0900

    start 를 인덱스로 지정해서 visited 처리

--- Coding_Test_Practice/BF재귀_BOJ14501_퇴사.cpp
+++ Coding_Test_Practice/BF재귀_BOJ14501_퇴사.cpp
@@ -5,63 +5,48 @@
 using namespace std;
 
 int N;
-int T[16] = {0};
-int P[16] = {0};
-int visited[16] = {0};
+int T[15] = {0};
+int P[15] = {0};
 
-int sum = 0;
 int ans = 0;
-vector<int> sumVec;
 
-void dfs(int chk){
+void dfs(int start, int sum){
     
+    if(start>N) return;
     
+    ans = max(ans, sum);
     
-    for(int i = 1; i<=N+1; i++){
-        
-        if(i>N){
-            ans = max(ans, sum);
-            return;
-        }
-        
-        if(visited[i]==0){
-            
-            if(i+T[i]-1>N) continue;
-            
-            for(int j = i; j<i+T[i]; j++){
-                visited[j] = 1;
-            }
-            sum+=P[i];
-            dfs(i);
-            sum-=P[i];
-            for(int j = i; j<i+T[i]; j++){
-                visited[j] = 0;
-            }
-            
-            
-            
-        }
-        
+    for(int i = start; i<N; i++){
+        dfs(i+T[i], sum+P[i]);
     }
-
-    
-    
+        
+        
+//        if(visited[i]==0){
+//
+//            if(i+T[i]-1>N) continue;
+//
+//            for(int j = i; j<i+T[i]; j++){
+//                visited[j] = 1;
+//            }
+//            sum+=P[i];
+//            dfs(i);
+//            sum-=P[i];
+//            for(int j = i; j<i+T[i]; j++){
+//                visited[j] = 0;
+//            }
+//        }
+        
 }
 
 
 int main(){
     
     cin >> N;
-    for(int i = 1; i<=N; i++){
+    for(int i = 0; i<N; i++){
         cin>> T[i] >> P[i];
     }
     
-    dfs(0);
-    
-    
+    dfs(0,0);
     cout << ans << "\n";
-    
-    
-    
     return 0;
 }
