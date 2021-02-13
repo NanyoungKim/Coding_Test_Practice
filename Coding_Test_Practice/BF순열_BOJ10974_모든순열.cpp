ea18f0300470f21efceebfccbed018588bb2cdc4
Nanyoung Kim <nanyoung18@gmail.com>
2021-02-13 16:32:22 +0900

    next_permutation -> prev_permutation

---
+++ Coding_Test_Practice/BF순열_BOJ10974_모든순열.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+vector<int> vec;
+
+int main(){
+
+
+    cin >> N;
+    for(int i = 0; i<N; i++){
+        vec.push_back(i+1);
+    }
+
+    do{
+        for(int i = 0; i<N; i++){
+            cout << vec[i] << " ";
+        }cout << "\n";
+        
+    } while(next_permutation(vec.begin(), vec.end()));
+
+
+    return 0;
+}
+
