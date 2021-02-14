4ef4b8a8e5b13d41bf0d5c5e14a1dfbccb7581b7
Nanyoung Kim <nanyoung18@gmail.com>
2021-02-14 22:40:44 +0900

    2/14

---
+++ Coding_Test_Practice/DP1_BOJ11727_2xn타일링2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int dp[1001] = {0};
+
+int main(){
+    
+    int n;
+    cin >> n;
+    
+    for(int i = 1; i<=n; i++){
+        
+    }
+    
+    
+    return 0;
+}
+
