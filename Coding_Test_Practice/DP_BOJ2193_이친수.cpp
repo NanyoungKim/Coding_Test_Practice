58ad636e4bfcf687d5dd7e7d6605194e22d6d1b8
Nanyoung Kim <nanyoung18@gmail.com>
2021-02-18 17:07:05 +0900

    자료형 long long

---
+++ Coding_Test_Practice/DP_BOJ2193_이친수.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+//자료형 int로 하면 통과 안 됨! 
+long long dp[91][2];
+int main(){
+    int T; cin >> T;
+    
+    for(int i = 0; i<=90; i++){
+        for(int j = 0; j<=1; j++){
+            dp[i][j] = 0;
+        }
+    }
+    
+    dp[1][0] = 0; dp[1][1] = 1;
+    
+    for(int i = 2; i<=90; i++){
+        dp[i][0] = dp[i-1][0] + dp[i-1][1];
+        dp[i][1] = dp[i-1][0];
+        //cout << dp[i][0] << " " << dp[i][1] << endl;
+    }
+    for(int i = 0; i<T; i++){
+        int N; cin >> N;
+
+        cout << dp[N][0] + dp[N][1];
+
+    }
+    
+    return 0;
+}
--- Coding_Test_Practice/DP_BOJ2225_합분해.cpp
+++ Coding_Test_Practice/DP_BOJ2225_합분해.cpp
@@ -2,3 +2,47 @@
 using namespace std;
 
 
+//N = 1~200
+int dp[201][201];
+
+int main(){
+    
+    for(int i = 1; i<=200; i++){
+        for(int j = 1; j<=200; j++){
+            dp[i][j] = 0;
+            if(j==1) { dp[i][j] = 1; continue;}
+        }
+    }
+    
+   
+    
+
+    int N, K;
+    cin >> N >> K;
+
+
+    for(int i = 2; i<=N; i++){
+        for(int j = 2; j<=i; j++){
+
+            for(int k = i-1; k>=j-1; k--){
+                dp[i][j] += dp[k][j-1] % 1000000000;
+            }
+            if(j==2) {dp[i][j] += 2; dp[i][j] %= 1000000000;}
+        }
+    }
+
+    cout << dp[N][K] << endl;
+    
+   
+//    for(int i = 1; i<=N; i++){
+//              for(int j = 1; j<=N; j++){
+//                  cout << dp[i][j] << " ";
+//           }
+//           cout << endl;
+//       }
+
+    
+    
+    
+    return 0;
+}
