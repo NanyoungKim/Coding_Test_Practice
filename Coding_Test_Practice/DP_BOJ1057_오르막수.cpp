63cbe4665f60041351d69b45cf798ee2d6dadfd7
Nanyoung Kim <nanyoung18@gmail.com>
2021-03-01 15:12:02 +0900

    DFS, depth==4,

---
+++ Coding_Test_Practice/DP_BOJ1057_오르막수.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[1001] = {0};
+
+int main(){
+    
+    int n; cin>>n;
+    
+    dp[0] = 0;
+    dp[1] = 10;
+    
+    
+    
+    
+    for(int i=2; i<=n; i++){
+        for(int j = 1; )
+    }
+    
+    
+    
+    return 0;
+}
--- 큐와 그래프/13023_ABCDE.cpp
+++ 큐와 그래프/13023_ABCDE.cpp
@@ -13,21 +13,17 @@ void dfs(int start, int depth){
     visited[start] = 1;
     
     if(depth==4){
-        flag = true;
+        flag = true;                //더 이상 dfs 실행 안 되게 함.
         cout << 1 << "\n";
         return;
-        
     }
-    
     
     for(int i = 0; i<vecArr[start].size(); i++){
-        if(visited[vecArr[start][i]]==0 && !flag){
-            dfs(vecArr[start][i], depth+1);
+        if(visited[vecArr[start][i]]==0 && !flag){        //위에서 flag=true 됐으면 조건문 실행 x
+            dfs(vecArr[start][i], depth+1);                //타고 들어갈 때마다 depth 증가
         }
     }
-    
-    
-    
+
     visited[start] = 0;
     
 }
@@ -43,7 +39,7 @@ int main(){
     cin >> N >> M;
     
     int a, b;
-    for(int i = 0; i<M; i++){
+    for(int i = 0; i<M; i++){            //관계 양 방향 연결 (벡터 배열 사용)
         cin >> a >> b;
         
         vecArr[a].push_back(b);
@@ -53,15 +49,10 @@ int main(){
     
     
     for(int i = 0; i<N; i++){
-        
-        
         dfs(i,0);
-        
     }
     
-    if(!flag) cout << 0;
-    
-    
+    if(!flag) cout << 0;                //다 탐색 했는데 깊이가 4 된 경우 없으면 0 출력.
     
     return 0;
 }
