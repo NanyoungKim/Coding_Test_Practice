82adbfc56a6b9805e578de2ee71d3ee3c7ffbd03
Nanyoung Kim <nanyoung18@gmail.com>
2021-03-03 00:21:33 +0900

    입력받을 때 주의, DFS

--- DFS/DFS_BOJ2667_단지번호붙이기.cpp
+++ DFS/DFS_BOJ2667_단지번호붙이기.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
 
 using namespace std;
 
 int map[25][25];
 int visited[25][25];
+vector<int> cntVec;
 
 //위 오 아 왼
 int dr[4] = {-1,0,1,0};
 int dc[4] = {0,1,0,-1};
 int N;
-void dfs(int r, int c, int cnt){
+int cnt;
+void dfs(int r, int c){
     
     
     
@@ -22,11 +27,9 @@ void dfs(int r, int c, int cnt){
         if(nr>=N || nr<0 || nc>=N || nc<0) continue;
         
         if(visited[nr][nc]==0 && map[nr][nc]==1){
-            
             visited[nr][nc] = 1;
-            dfs(nr,nc, cnt+1);
-            visited[nr][nc] = 0;
-            
+            cnt+=1;
+            dfs(nr,nc);
         }
     }
  
@@ -40,28 +43,43 @@ int main(){
     int res=0;
     
     cin >> N;
+    string str;
     for(int i = 0; i<N; i++){
-        for(int j = 0; j<N; j++){
-            cin >> map[i][j];
+        cin >> str;
+        for(int j = 0; j<str.length(); j++){
             visited[i][j] = 0;
+            
+            if(str[j] == '1'){
+                map[i][j] = 1;
+            }
+            else map[i][j] = 0;
         }
+        
+    
     }
     
-    
+
     
     for(int i = 0; i<N; i++){
         for(int j = 0; j<N; j++){
             
             if(map[i][j]==1 && visited[i][j]==0){
                 visited[i][j] = 1;
-                dfs(i,j, 0);
-                visited[i][j] = 0;
+                cnt = 1;
+                dfs(i,j);
+                cntVec.push_back(cnt);
                 res++;
             }
         }
     }
     
-    cout << cnt;
+    sort(cntVec.begin(), cntVec.end());
+    cout << res << "\n";
+    
+    
+    for(int i = 0; i<cntVec.size(); i++){
+        cout << cntVec[i] << "\n";
+    }
     
     
     return 0;
