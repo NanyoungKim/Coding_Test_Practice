b1f74577c7a22f4f845ec5116d1f90ad70eacacd
Nanyoung Kim <nanyoung18@gmail.com>
2021-07-04 19:58:43 +0900

    연결 그래프의 간선 수 : 노드 수 -1

---
+++ Tree/Tree_BOJ9372_상근이의여행.cpp
@@ -0,0 +1,29 @@
+//
+//  Tree_BOJ9372_상근이의여행.cpp
+//  Coding_Test_Practice
+//
+//  Created by 김난영 on 2021/07/03.
+//  Copyright © 2021 KimNanyoung. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int T; cin >> T;
+    for(int test_case = 1; test_case<=T; test_case++){
+        
+        int N, M;
+        cin >> N >> M;
+        int a, b;
+        for(int i = 0; i<M; i++){
+            cin >> a >> b;
+            
+        }
+        cout << N-1 << "\n";
+        
+    }
+    
+    return 0;
+}
